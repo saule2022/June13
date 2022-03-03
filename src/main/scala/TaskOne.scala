46521cd02809c274fd9ac7feb88940d610921e25
jsaule <julija.sauleniene@gmail.com>
2022-03-03 21:03:04 +0200

    Review 2

--- src/main/scala/TaskOne.scala
+++ src/main/scala/TaskOne.scala
@@ -2,7 +2,6 @@ import scala.io.StdIn.readLine
 object TaskOne extends App {
     val yourName = readLine("Hi, what is your name? ")
     val yourAge = readLine(s"Nice to meet you, $yourName. How old are you? ")
-    val year = 2022
     val yearHundret = 100 - yourAge.toInt + 2022
     println(s"You will be 100 years ol in $yearHundret.")
 }
