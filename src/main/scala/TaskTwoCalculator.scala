383c8cde9627f26ba40f2b6f10ac56fe1fe704a4
jsaule <julija.sauleniene@gmail.com>
2022-03-07 20:49:13 +0200

    First commit

--- src/main/scala/TaskTwoCalculator.scala
+++ src/main/scala/TaskTwoCalculator.scala
@@ -1,15 +1,16 @@
 import scala.io.StdIn.readLine
 
-object TaskTwoCalculator {
+object TaskTwoCalculator extends App {
 println("Xmas bonus calculator")
   val yourName = readLine("Hi, what is your name? ")
   val yourYears = readLine(s"Nice to meet you, $yourName. How many years have you been working at the firm? ")
-  val yourWage = readLine("What your monthly wage would be? ")
   if (yourYears.toInt < 2) {
-    println("sorry, no bonus this year")
+    println("Sorry, no bonus this year")
   }
   else if (yourYears.toInt >= 2) {
-    val bonus = yourWage.toDouble * 1.15
-    println(s"your bonus this year is $bonus")
+    val yourWage = readLine("What your monthly wage would be? ")
+    val bonus = yourWage.toDouble * 0.15 * (yourYears.toDouble - 2)
+    val bonusI = bonus.toInt
+    println(s"Your bonus this year is $bonusI.")
   }
 }
