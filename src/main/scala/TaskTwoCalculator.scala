1826d2ee702d35182b4ff4d425e5d3c835cbdb8e
jsaule <julija.sauleniene@gmail.com>
2022-03-07 21:00:24 +0200

    Commit

--- src/main/scala/TaskTwoCalculator.scala
+++ src/main/scala/TaskTwoCalculator.scala
@@ -5,7 +5,7 @@ println("X-mas bonus calculator")
   val yourName = readLine("Hi, what is your name? ")
   val yourYears = readLine(s"Nice to meet you, $yourName. How many years have you been working at the firm? ")
   if (yourYears.toInt < 2) {
-    println("Sorry, no bonus this year")
+    println("Sorry, no bonus this year.")
   }
   else if (yourYears.toInt >= 2) {
     val yourWage = readLine("What your monthly wage would be? ")
