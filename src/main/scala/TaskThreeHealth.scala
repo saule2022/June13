90478968abb14a4ee8ddb77f80f47f66e36c2dfa
jsaule <julija.sauleniene@gmail.com>
2022-03-08 12:14:03 +0200

    Temperature Exercise

--- src/main/scala/TaskThreeHealth.scala
+++ src/main/scala/TaskThreeHealth.scala
@@ -3,10 +3,10 @@ object TaskThreeHealth extends App {
   println("Temperature exercise")
   val yourTemperature = readLine("Hi, what is your temperature? ").toDouble
   if (yourTemperature < 35) {
-    println("That is a bit too cold")
+    println("That is a bit too cold.")
   }
   else if (yourTemperature >= 35 && yourTemperature <= 37) {
     println("You are all right!")
   }
-    else println("You have a fever! Consider contacting a doctor")
+    else println("You have a fever! Consider contacting a doctor.")
 }
