889cc047fc59d79c238a0a079ac25d9eda7e8864
jsaule <julija.sauleniene@gmail.com>
2022-03-21 12:04:02 +0200

    FizzBuzz

---
+++ src/main/scala/TaskFourFizzBuzz.scala
@@ -0,0 +1,17 @@
+import scala.io.StdIn.readLine
+object TaskFourFizzBuzz extends App{
+  val fizz = 5
+  val buzz = 7
+  val minValue = 1
+  val maxValue = 100
+for (x <- minValue to maxValue) {
+  if (x % fizz == 0 && x % buzz == 0)
+    print("FizzBuzz, ")
+  else if (x % buzz == 0)
+    print("Buzz, ")
+  else if (x % fizz == 0)
+    print("Fizz, ")
+  else
+    print(x + ", ")
+}
+}
