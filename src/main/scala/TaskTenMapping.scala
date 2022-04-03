7321ba001c4f6ac13eb5f484f542098805fc75bd
Julija Sauleniene <julija.sauleniene@gmail.com>
2022-04-03 22:17:37 +0300

    Words and characters count

--- src/main/scala/TaskTenMapping.scala
+++ src/main/scala/TaskTenMapping.scala
@@ -1,13 +1,29 @@
 import TaskNineWordLength.wordLengths
 
 import scala.io.StdIn.readLine
-import scala.collection.mutable.Map
 
-object TaskTenMapping extends App{
+object TaskTenMapping extends App {
+  //counts words *immutable by default
   val sentence = readLine("Please enter sentence. ")
   val words = sentence.split(" ")
 
   val wordLengths = words.map(words => words.length)
   val wordsMap = Map(words.mkString(", ") -> wordLengths.mkString(", "))
-  println(wordsMap)
+
+  val wordsLengthArray = for ((word, length) <- words zip wordLengths) yield word -> length
+  println(wordsLengthArray.mkString(","))
+  println(wordsLengthArray.length)
+  val realWordsLengthMap = wordsLengthArray.toMap
+  println(realWordsLengthMap)
+
+  //counts characters
+
+  def countCharsByCounting(text: String):Map[Char, Int] = {
+    val charCountMap = scala.collection.mutable.Map[Char, Int]()
+    for (c <- text) {
+      charCountMap += (c -> text.count(_ == c))
+    }
+    charCountMap.toMap
+  }
+  println(countCharsByCounting("let's see what we have here"))
 }
