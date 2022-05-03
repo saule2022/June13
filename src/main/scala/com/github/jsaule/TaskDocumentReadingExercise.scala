3c7084ee57315ac10367618f3dae19c8256120df
Julija Sauleniene <julija.sauleniene@gmail.com>
2022-05-03 09:53:54 +0300

    Version 1

--- src/main/scala/com/github/jsaule/TaskDocumentReadingExercise.scala
+++ src/main/scala/com/github/jsaule/TaskDocumentReadingExercise.scala
@@ -1,15 +1,19 @@
 package com.github.jsaule
+
 import scala.io.Source
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.io.FileWriter
 
 class Document(title:String = "", author:String = "", url:String = "", rows:Array[String] = Array[String]()) {
   val rowCount: Int = rows.length
   val wordCount: Int = JulUtil.getWordCountPerLine(rows).sum
-  val firstRow = s"URL: $url"
-  val secondRow = s"Author: $author"
-  val thirdRow = s"Title: $title"
-  val threeNewLines = s"'\n'* 3"
+  val firstRow = s"URL: $url \n"
+  val secondRow = s"Author: $author \n"
+  val thirdRow = s"Title: $title \n"
+  val threeNewLines = s"\n\n\n"
+  val folderForDocuments = "src/resources/texts/"
+  val documentURLs = "src/resources/texts/webPages.txt"
 
   def addTimeStampToUrl(): String = {
     val currentDateTime = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm").format(LocalDateTime.now())
@@ -17,44 +21,44 @@ class Document(title:String = "", author:String = "", url:String = "", rows:Arra
   }
 
   def createDocumentName: String = {
-    val author = JulUtil.GutenbergUtil.getAuthor(rows).replace(' ', '_')
-    val title = JulUtil.GutenbergUtil.getTitle(rows).replace(' ', '_')
-    author.slice(0, 10) + "_" + title.slice(0, 15) + "_" + addTimeStampToUrl + ".txt"
+    val authorDoc = author.replace(' ', '_')
+    val titleDoc = title.replace(' ', '_')
+    authorDoc.slice(0, 10) + "_" + titleDoc.slice(0, 15) + "_" + addTimeStampToUrl + ".txt"
   }
 
-  def getDataFromWeb(url: String = ""): Array[String] = {
+  def saveURL(documentURLs: String, url: String, append:Boolean=true, verbose:Boolean=true): Unit = {
+    if (verbose) println(s"Saving URL $url to $documentURLs done.")
+    val fw = new FileWriter(documentURLs, append)
+    if (append) fw.write("\n")
+    fw.write(url)
+    fw.close()
+  }
+
+  saveURL(documentURLs, url)
+
+  def getDataLinesFromURL(url: String = ""): String = {
     val bufferedSource = Source.fromURL(url)
-    val dataLines = bufferedSource.getLines().toArray
-    bufferedSource.close()
-    dataLines
+    bufferedSource.mkString
   }
 
-  def combinedLines: Array[String] = {
-    val contentLines: Array[String] = Array(firstRow, secondRow, thirdRow, threeNewLines) ++ rows
-    contentLines
-  }
+  val text: String = getDataLinesFromURL(url)
+  val rowsALL: String = firstRow + secondRow + thirdRow + threeNewLines + text
+  val dst: String = folderForDocuments + createDocumentName
 
-  def save(dst: String = "", folder: String = "src/resources/texts"): Unit = {
-    val lines = getDataFromWeb(url)
-    val documentPath: String = if (dst == "") folder + "/" + createDocumentName else folder + "/" + dst
-    JulUtil.saveLines(documentPath, lines)
+  def saveDocument(dst: String, rowsAll: String, append:Boolean=false, verbose:Boolean=true): Unit = {
+    if (verbose) println(s"Saving document to $dst done.")
+    val fw = new FileWriter(dst, append)
+    if (append) fw.write("\n")
+    fw.write(rowsAll)
+    fw.close()
   }
+  saveDocument(dst, rowsALL)
+
   Thread.sleep(200)
 }
 
 object TaskDocumentReadingExercise extends App {
 
-  println("start")
-
-  var filePath = ""
-  if (args.length == 0) {
-    filePath = "src/resources/texts/webPages.txt"
-  } else {
-    filePath = args(0)
-  }
-  val getUrls = JulUtil.getLinesFromFile(filePath)
-  val urlList = for (line <- getUrls) yield {
-    if (line.startsWith("http://") || line.startsWith("https://")) line
-    else "https://" + line
-  }
+  val test0Document = new Document("Phaedo", "Plato", "https://gutenberg.org/cache/epub/1658/pg1658.txt")
+  val test1Document = new Document("Protagoras", "Plato", "https://gutenberg.org/cache/epub/1591/pg1591.txt")
 }
