ae0d7f94e574e198d74d328a9b4db934f26957ab
saule2022 <99681344+saule2022@users.noreply.github.com>
2022-06-14 21:12:39 +0300

    Day 2 Exercise

--- June13/src/main/scala/Day2Excercise.scala
+++ June13/src/main/scala/Day2Excercise.scala
@@ -17,7 +17,6 @@ object Day2Exercise extends App {
   //print product(multiplication) of odd squares of numbers from 1 to user input
   //so user enters 5
   //answer would be 1*9*25 = 225
-
   //so for reduce part you should define mult function or you could write it inline but that is a bit harder
   //goal would be to use view map filter reduce in a single line
   //you can do it first in separate operations of course
